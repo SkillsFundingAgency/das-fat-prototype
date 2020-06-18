44e0ced3b0211e7be2a2eb81ba6a24a04ace23d1
Dave Haigh <davehaigh@outlook.com>
2020-06-18 16:58:49 +0100

    8-0 training options

--- app/routes/8-0/routes.js
+++ app/routes/8-0/routes.js
@@ -966,10 +966,10 @@ module.exports = function (router,_myData) {
             });
     
             if(req.session.myData.locationapplied){
-                if(req.session.myData.sortby == "name"){
-                    sortProviders(req, "name")
-                } else {
+                if(req.session.myData.sortby == "distance"){
                     sortProviders(req, "distance")
+                } else {
+                    sortProviders(req, "name")
                 }
             } else {
                 sortProviders(req, "name")

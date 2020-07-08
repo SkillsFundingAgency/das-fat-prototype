b18e607dd7dc7656d5fd3d2d3e55c5131a442325
Dave Haigh <davehaigh@outlook.com>
2020-07-08 10:36:09 +0100

    test code commented out

--- app/routes.js
+++ app/routes.js
@@ -123,7 +123,17 @@ for (var _epaoLarsCode in _epaosOnStandards) {
 }
 _myData.epaosOnStandardsCounts = _epaosOnStandardsCounts
 
-// for pulling out counts of statuses on all stds in IFATE api
+
+
+
+
+//
+//
+// FOR DOING TESTS ON IFATE DATA
+//
+//
+
+// // for pulling out counts of statuses on all stds in IFATE api
 // _myData.testStandardsData = []
 // console.log("test")
 // require("request").get("https://www.instituteforapprenticeships.org/api/apprenticeshipstandards", (error, response, body) => {
@@ -146,15 +156,61 @@ _myData.epaosOnStandardsCounts = _epaosOnStandardsCounts
 //             _testStandardsData.push(_standardData)
 //         }
 //         if(_standard.coreAndOptions && _standard.skills.length > 0){
-//             console.log(_title + " has " + _standard.skills.length + " skills.")
+//             // console.log(_title + " has " + _standard.skills.length + " skills.")
 //         }
+
+
+//         // FOR COMPARING ARRAYS
+//         // Warn if overriding existing method
+//         // if(Array.prototype.equals)
+//         // console.warn("Overriding existing Array.prototype.equals. Possible causes: New API defines the method, there's a framework conflict or you've got double inclusions in your code.");
+//         // attach the .equals method to Array's prototype to call it on any array
+//         Array.prototype.equals = function (array) {
+//             // if the other array is a falsy value, return
+//             if (!array)
+//                 return false;
+
+//             // compare lengths - can save a lot of time 
+//             if (this.length != array.length)
+//                 return false;
+
+//             for (var i = 0, l=this.length; i < l; i++) {
+//                 // Check if we have nested arrays
+//                 if (this[i] instanceof Array && array[i] instanceof Array) {
+//                     // recurse into the nested arrays
+//                     if (!this[i].equals(array[i]))
+//                         return false;       
+//                 }           
+//                 else if (this[i] != array[i]) { 
+//                     // Warning - two different object instances will never be equal: {x:20} != {x:20}
+//                     return false;   
+//                 }           
+//             }       
+//             return true;
+//         }
+//         // Hide method from for-in loops
+//         Object.defineProperty(Array.prototype, "equals", {enumerable: false});
+//         if(!_standard.typicalJobTitles.equals(_standard.jobRoles)){
+//             console.log(_title + " - DONT MATCH")
+//             console.log(_standard.typicalJobTitles)
+//             console.log(_standard.jobRoles)
+//         }
+//         // console.log("checked")
 //         // console.log(_title)
+
+        
 //     });
 //     // console.log("START")
 //     // console.log(JSON.stringify(_testStandardsData))
 //     // console.log("END")
 // });
 // // var distinctStatuses = [...new Set(_apiData.map(function(data){return data.statuses}))];
+
+
+
+
+
+
 
 // Set ssa + route counts
 // Regulated

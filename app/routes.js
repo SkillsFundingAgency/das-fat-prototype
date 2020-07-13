9c85d2a35288bb35231fe4fc25e5949b241ac519
Dave Haigh <davehaigh@outlook.com>
2020-07-13 09:20:34 +0100

    version 10 created

--- app/routes.js
+++ app/routes.js
@@ -439,6 +439,7 @@ require('./routes/6-0/routes.js')(router,JSON.parse(JSON.stringify(_myData)));
 require('./routes/7-0/routes.js')(router,JSON.parse(JSON.stringify(_myData)));
 require('./routes/8-0/routes.js')(router,JSON.parse(JSON.stringify(_myData)));
 require('./routes/9-0/routes.js')(router,JSON.parse(JSON.stringify(_myData)));
+require('./routes/10-0/routes.js')(router,JSON.parse(JSON.stringify(_myData)));
 
 
 module.exports = router
